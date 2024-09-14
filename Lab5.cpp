5415ff0e3efff9b966ee5e6bcceed03cebab38c1
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-09-14 13:17:26 -0700

    Timestamp

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int swap(int &, int &);
+// int swap(int &, int &);
 int swap(int *, int *);
 
 int main()
@@ -17,23 +17,30 @@ int main()
     cout << "Swapping...\n";
     // int sum = swap(x, y);
     int sum = swap(xPtr, yPtr);
-    cout << "x = " << x << " | y = " << y << endl;
+    // cout << "x = " << x << " | y = " << y << endl;
+    cout << "xPtr = " << xPtr << "  | yPtr = " << yPtr << endl;
     cout << "sum = " << sum << endl;
+
+    delete xPtr, yPtr;
+    xPtr = nullptr;
+    yPtr = nullptr;
+
     return 0;
 }
 
-int swap(int &a, int &b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-    return a + b;
-}
+// int swap(int &a, int &b)
+// {
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     return a + b;
+// }
 
 int swap(int *a, int *b)
 {
-    int temp = a;
+    int *temp = a;
     a = b;
     b = temp;
-    return a + b;
+    temp = nullptr;
+    return *a + *b;
 }
